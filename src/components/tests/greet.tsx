7ba2ae1122864837b0731999e76b61ded3ca0340
Sun Sreng <sun_sreng@hotmail.com>
2024-11-29 19:04:36 +0700

    feat: remove test

--- src/components/tests/greet.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-export const Greet: FC<{ name?: string }> = ({ name }) => {
-  if (name) return <h1 data-testid="greet">Hello {name}</h1>;
-
-  return <button data-testid="btn-login">Login</button>;
-};
-
-export default Greet;
