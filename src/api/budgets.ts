cc5878b5b868ca376dc375b4ad025a3e8447ad44
Sun Sreng <sun_sreng@hotmail.com>
2024-11-29 19:36:31 +0700

    feat: update mock data to host

--- src/api/budgets.ts
+++ src/api/budgets.ts
@@ -1,15 +1,18 @@
+import { budgets } from '@/assets/mock/budgets';
 import { IBudgetCard } from '@/types/budget-card';
 
 export async function getBudgets(status?: 'active' | 'achieved') {
-  const response = await fetch('http://localhost:3001/budgets');
-  const data: IBudgetCard[] = await response.json();
+  // const response = await fetch('http://localhost:3001/budgets');
+  // const data: IBudgetCard[] = await response.json();
 
-  if (status) return data.filter((budget) => budget.status === status);
+  if (status) return budgets.filter((budget) => budget.status === status);
 
-  return data;
+  return budgets;
 }
 
 export async function getBudgetByID(id: string): Promise<IBudgetCard> {
-  const response = await fetch(`http://localhost:3001/budgets/${id}`);
-  return await response.json();
+  // const response = await fetch(`http://localhost:3001/budgets/${id}`);
+  // return await response.json();
+
+  return budgets.find((budget) => budget.id === id)!;
 }
